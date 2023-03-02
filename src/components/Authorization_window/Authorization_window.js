e75af9750bb5f31106915c8cf834ab08a0298bd7
elitanima <elitanima@vk.com>
2023-03-02 17:01:14 +0300

    Added formik autorization and registration

--- src/components/Authorization_window/Authorization_window.js
+++ src/components/Authorization_window/Authorization_window.js
@@ -2,25 +2,39 @@
 
 import style from './style.module.css'
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+
 import { useNavigate } from "react-router-dom";
 
 
 export function Authorization_window() {
 
     const navigate = useNavigate();
-
+   
     return (
-        <div className={style.hidden}>
-            <div className={style.end_modal_avtorization}>  
-                <div className={style.modal}>
-                    <form className={style.form_avtorization} name="form_add_god" id="formAddGod"> 
-                        <input className={style.form_data} type="email" name="email" id="email" placeholder="Email"></input>
-                        <input className={style.form_data} type = "password" name="userPassword" id="userPassword" placeholder="Введите пароль" min="1"></input>
-                        <button className={style.form_data} data-btn="entry">Войти</button>
-                        <span className={style.form_data} data-btn="formAutorizationClose" onClick={() => navigate("/")}>Отмена</span>
-                    </form>
+        <Formik
+        initialValues={{ email: "", password: ""}}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+            <div className={style.hidden}>
+                <div className={style.end_modal_avtorization}>  
+                    <div className={style.modal}>
+                        <Form className={style.form_avtorization} >
+                                <Field className={style.form_data} name="email"  type="email" placeholder="Email"/>
+                                <Field className={style.form_data} name="password" type="password" placeholder="Пароль" />
+                                <button className={style.form_data} type="submit">Войти</button>
+                                <span className={style.form_data} data-btn="formAutorizationClose" onClick={() => navigate("/")}>Отмена</span>
+                        </Form>
+                    </div>
                 </div>
             </div>
-        </div>
+
+      </Formik>
     )   
 }
+
--- src/components/Registration_window/Registration_window.js
+++ src/components/Registration_window/Registration_window.js
@@ -5,31 +5,39 @@ import style from './style.module.css';
 
 import { useNavigate } from "react-router-dom";
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+
+
 export function Registration_window() {
 
     const navigate = useNavigate();
 
     return (
-        <div className={style.hidden} data-modal="id_modal_add_god">
-            <div className={style.end_modal_registration}>  
-                <div className={style.modal}>
-                    <form className={style.form_avtorization} name="form_add_god" id="formAddGod"> 
-                        <input className={style.form_data} type="email" name="email" id="email" placeholder="Email"></input>
-                        <input className={style.form_data} type = "password" name="userPassword" id="userPassword" placeholder="Придумайте пароль" min="1"></input>
-                        <input className={style.form_data} type="text" name="group" id="group" placeholder="Группа"></input>
-                        <button className={style.form_data} data-btn="entry">Регистрация</button>
-                        <span className={style.form_data} data-btn="formAutorizationClose" onClick={() => navigate(-1)}>Отмена</span>
-                    </form>
+        <Formik
+            initialValues={{ email: "", password: "", group: ""}}
+            onSubmit={async (values) => {
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            alert(JSON.stringify(values, null, 2));
+        
+            }}
+>
+            <div className={style.hidden}>
+                <div className={style.end_modal_registration}>  
+                    <div className={style.modal}>
+                        <Form className={style.form_avtorization} >
+                                <Field className={style.form_data} name="email"  type="email" placeholder="Email"/>
+                                <Field className={style.form_data} name="password" type="password" placeholder="Придумайте пароль" />
+                                <Field className={style.form_data} name="group" type="text" placeholder="Группа" />
+                                <button className={style.form_data} type="submit">Регистрация</button>
+                                <span className={style.form_data} data-btn="formAutorizationClose" onClick={() => navigate(-1)}>Отмена</span>
+                        </Form>
+                    </div>
                 </div>
             </div>
-        </div>
+
+        </Formik>
     )   
 }
 
-// {/* <div className={style.hidden} data-modal="id_modal_add_god">
-// <div className={style.end_modal_registration}>  
-//     <div className={style.modal}>
-        
-//     </div>
-// </div>
-// </div> */}
