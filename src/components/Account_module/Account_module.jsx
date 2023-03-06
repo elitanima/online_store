c1a87c8471040fc8ceb7f31f3b90e8e758063d34
elitanima <elitanima@vk.com>
2023-03-06 12:43:14 +0300

    Server response processing (personal account)

--- src/components/Account_module/Account_module.jsx
+++ src/components/Account_module/Account_module.jsx
@@ -3,21 +3,12 @@
 import style from './style.module.css'
 import { useNavigate } from "react-router-dom";
 
-import { user } from "../../api_user/api_user"
+
 
 export function Account_module() {
     
     const navigate = useNavigate();
-    const token = localStorage.getItem('token')
-
-     // Отрываем страницу профиля
-        const goToUser = async (token) => {
-        const res = await user(token);
-        const data = await res.json();
-        console.log(data);
-        navigate("/profile")
-    }
-    
+   
 
     const exit = () => {
         localStorage.clear()
@@ -27,7 +18,7 @@ export function Account_module() {
     return (
          <div className={style.account_module}>
             <div className={style.button_view}>
-                <button className={style.button_user} onClick={() => goToUser(token)}></button> 
+                <button className={style.button_user} onClick={() => navigate("/profile")}></button> 
                 <button className={style.button_basket} onClick={() => navigate("/basket")}></button>
                 <button className={style.button_exit} onClick={() => exit() }></button>
          </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,39 @@
 import { Account_module } from '../Account_module/Account_module'
 import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
+import { user } from "../../api_user/api_user"
 import style from './style.module.css'
 
 export function Profile() {
+
+   
+
+    const token = localStorage.getItem('token')
+
+     // Отрываем страницу профиля
+        const goToUser = async (token) => {
+        const res = await user(token);
+        const data = await res.json();
+        let $profile = document.querySelector('#profile');
+        $profile.insertAdjacentHTML("beforeend", profilePage(data));
+    }
+    goToUser(token)
+
+    // формирование html
+let profilePage = (data) => 
+`<div>
+    <h3>${data.name}</h3>
+    <h3>${data.about}</h3>
+    <h3>${data.avatar}</h3>
+</div>`
+    
     return (
         <div className={style.container}>
             <div className={style.screenView}>
                 <Header />
-                    <div className={style.profile}>
+                    <div className={style.profile} id='profile'>
                         <h1>Личный кабинет</h1>
+                    
                     </div>
                 <Footer />
             </div> 
