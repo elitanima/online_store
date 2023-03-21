f4617adc3cf5c1b9c65fdbfab8c717a8bb540872
elitanima <elitanima@vk.com>
2023-03-21 16:32:00 +0300

    minor changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 
+import { useLocation } from 'react-router'
 import { AccountButton } from '../AccountButton/AccountButton'
 import { Search } from '../Search/Search'
 import style from './style.module.css'
 
 
 export function Header() {
-
+    const location = useLocation()
     return (
         <header className={style.header}>
-            <Search />
+            { location.pathname ==='/main' ? <Search /> :null }
             <AccountButton />
         </header>
     )   
