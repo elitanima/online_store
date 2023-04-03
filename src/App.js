576c89940126de64799372ded166e887d807117a
elitanima <elitanima@vk.com>
2023-04-03 13:08:15 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import style from './style.module.css';
 import { Header } from './components/Header/Header';
 import { Main } from './pages/Main/Main';
 import { Footer } from './components/Footer/Footer';
@@ -5,8 +6,8 @@ import { Footer } from './components/Footer/Footer';
 function App() {
 
   return (
-    <div className='container'>
-      <div className='screen_view'>
+    <div className={style.container}>
+      <div className={style.screen_view}>
         <Header />
         <Main />  
         <Footer />
