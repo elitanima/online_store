ee71c0e67743489f2bb61d0d65e60ee4d94b51d8
elitanima <elitanima@vk.com>
2023-04-03 18:52:29 +0300

    minor changes

--- src/components/CardSinglProduct/CardSinglProduct.jsx
+++ src/components/CardSinglProduct/CardSinglProduct.jsx
@@ -2,25 +2,14 @@ import { useDispatch } from 'react-redux';
 import style from './style.module.css'
 import { addToBasket } from "../../redux/slices/basket";
 import { addToFavorites } from '../../redux/slices/favorites';
-import { useNavigate } from 'react-router';
 
 export function CardSinglProduct({ oneProduct }){
     const dispatch = useDispatch();
-    const navigate = useNavigate()
   
-    const handleAddToBasket = (event) => {
-      event.stopPropagation()
-      dispatch(addToBasket(oneProduct._id))
-    }
-
-    const handleAddToFavorites = (event) => {
-      event.stopPropagation()
-      dispatch(addToFavorites(oneProduct._id))
-    }
     return (
       <>
-       <div className={style.card} onClick={() => navigate(oneProduct._id)}>
-            <button className={style.btn_favorites} onClick={(event) => handleAddToFavorites(event)}></button>
+       <div className={style.card} >
+            <button className={style.btn_favorites} onClick={() => dispatch(addToFavorites(oneProduct._id))}></button>
             <div className={style.picture}>
               <img alt="" src={oneProduct.pictures} />
             </div>
@@ -34,7 +23,7 @@ export function CardSinglProduct({ oneProduct }){
             <div >
               <h3 className={style.normal__price}>{oneProduct.price} ₽</h3>
             </div>
-            <button className={style.btn_basket} onClick={(event) => handleAddToBasket(event)}>В корзину</button>
+            <button className={style.btn_basket} onClick={() => dispatch(addToBasket(oneProduct._id))}>В корзину</button>
                     
         </div>
       </>
